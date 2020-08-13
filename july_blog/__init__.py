c72635cac96361aef25eb94921e7d1885b509d13
mlatona17 <mikelatona17@gmail.com>
2020-08-13 15:52:56 -0400

    First commit blog_july_flask_project

--- july_blog/__init__.py
+++ july_blog/__init__.py
@@ -2,8 +2,27 @@ from flask import Flask
 
 from config import Config
 
+# Import for Flask DB and Migrator
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Import for Flask Mail
+from flask_mail import Mail, Message
+
+# Import for Flask Login
+from flask_login import LoginManager
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
-from july_blog import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+mail = Mail(app)
+
+# Login Config
+login = LoginManager(app)
+login.login_view = 'login' # Specify what page to load for NON-authorized Users
+
+from july_blog import routes, models
--- july_blog/routes.py
+++ july_blog/routes.py
@@ -1,15 +1,118 @@
-from july_blog import app
-from flask import render_template
+from july_blog import app, db, Message, mail
+from flask import render_template, request, redirect, url_for
+
+# Import for Forms
+from july_blog.forms import UserInfoForm, BlogPostForm, LoginForm
+
+# Import for Models
+from july_blog.models import User, Post, check_password_hash
+
+# Import for Flask Login - login_required, login_user, current_user, logout_user
+from flask_login import login_required, login_user, current_user, logout_user
 
 # Home Route
 @app.route('/')
 def  home():
-    customer_name = "Mike"
-    order_number = 1
-    item_dict = {1: "Ice Cream", 2: "Bread", 3:"Lemons", 4: "Cereal"}
-    return render_template("home.html", customer_name=customer_name, order_number = order_number, item_dict = item_dict)
+    posts = Post.query.all()
+    return render_template("home.html", posts=posts)
 
 #Register Route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = UserInfoForm() #() = instance. MUST use
+    if request.method == 'POST' and form.validate():
+        # Get Information from POST request
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        print("\n", username, password, email)
+        # Create an instance of User
+        user = User(username, email, password)
+        # Open and insert into database
+        db.session.add(user)
+        # Save info to database
+        db.session.commit()
+
+        # Flask Email Sender
+        msg = Message(f'Thanks for signing up, {username}!', recipients=[email])
+        msg.body = ('Congrats on signing up! Looking forward to your posts!')
+        msg.html = ('<h1>Welcome to the July Blog!</h1>' '<p>This will be fun!</p>')
+
+        mail.send(msg)
+
+    return render_template("register.html", form=form)
+
+# Create Post route
+@app.route('/createposts', methods=['GET', 'POST'])
+@login_required
+def createposts():
+    form = BlogPostForm()
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        content = form.content.data
+        user_id = current_user.id
+        print("\n", title, content)
+        post = Post(title,content, user_id)
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('createposts'))
+    return render_template("createposts.html", form=form)
+
+@app.route('/posts/<int:post_id>')
+@login_required
+def post_detail(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_detail.html', post=post)
+
+@app.route('/posts/update/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def post_update(post_id):
+    post = Post.query.get_or_404(post_id)
+    update_form = BlogPostForm()
+
+    if request.method == 'POST' and update_form.validate():
+        title = update_form.title.data
+        content = update_form.content.data
+        user_id = current_user.id
+        
+        #Update post with form info
+        post.title = title
+        post.content = content
+        post.user_id = user_id
+
+        # Commit change to db
+        db.session.commit()
+        return redirect(url_for('post_update', post_id = post.id))
+
+    return render_template('post_update.html', update_form=update_form)
+
+
+@app.route('/posts/delete/<int:post_id>', methods=['POST'])
+@login_required
+def post_delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        password = form.password.data
+        logged_user = User.query.filter(User.email == email).first()
+        if logged_user and check_password_hash(logged_user.password, password):
+            login_user(logged_user)
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
